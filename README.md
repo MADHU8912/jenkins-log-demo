e8c75dbaa976170ca8f7ef88baed71818a2f672a
MADHU8912 <nikhilabba12@gmail.com>
2026-03-31 15:22:26 +0530

    Added Jenkins log demo project

---
+++ README.md
@@ -0,0 +1,2 @@
+Jenkins Log Demo 
+Build Log, Pipeline Log, Application Log using CMD + Git + Jenkins 
---
+++ app.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Application started...
+echo User login success 
+echo Data loaded 
+echo Application finished 
+type application.log
